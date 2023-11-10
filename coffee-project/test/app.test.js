97aae69e52a833208de3916d68c9453652112360
Deep Mehta <deep.mm@somaiya.edu>
2023-11-10 14:35:55 -0500

    Removing test to check code coverage

--- coffee-project/test/app.test.js
+++ coffee-project/test/app.test.js
@@ -20,30 +20,6 @@ describe('Coffee Delivery Service API', () => {
         });
     });
 
-    describe('POST /order', () => {
-        it('should place an order', (done) => {
-            request(app)
-                .post('/order')
-                .send({ coffeeId: 1, quantity: 2 })
-                .end((err, res) => {
-                    expect(res.statusCode).to.equal(201);
-                    expect(res.body).to.be.an('object');
-                    expect(res.body).to.have.property('orderId');
-                    done();
-                });
-        });
-
-        it('should return an error for invalid coffee ID', (done) => {
-            request(app)
-                .post('/order')
-                .send({ coffeeId: 999, quantity: 2 })
-                .end((err, res) => {
-                    expect(res.statusCode).to.equal(400);
-                    done();
-                });
-        });
-    });
-
     describe('GET /orders', () => {
         it('should return a list of placed orders', (done) => {
             request(app)
